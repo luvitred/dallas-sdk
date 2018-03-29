9d49c7bbb77bde7021becb30ff9eb035d75206a8
Timothy <t.vanwonterghem@option.com>
2018-03-29 11:40:31 +0200

    add cortex link

---
+++ lib/general/link/CORTEX/cortexow.h
@@ -0,0 +1,12 @@
+#ifndef __CORTEXOW__
+#define __CORTEXOW__
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/cortex_owire_ioctl.h>
+
+#define BITPOL_ONE                     0x10
+#define BITPOL_ZERO                    0x00
+
+#endif
